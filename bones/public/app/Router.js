19b808eb6b36c689073d8b9b238e1b0a99c619b9
Markiyan Matviyiv <mmatviyiv@morningcoffeesoft.com>
2013-04-25 18:02:12 +0300

    bootstrap demo

--- bones/public/app/Router.js
+++ bones/public/app/Router.js
@@ -4,13 +4,13 @@ window.App = new (Backbone.Router.extend({
 	},
 
 	index: function() {
-		alert('Hello');
+
 	},
 
 	initialize: function() {
 		console.log("App> Initializing routes...");
 		console.log("App> Routes " + JSON.stringify(this.routes, null, '\t'));		
-	}, 
+	},
 
 	start: function() {
 		Backbone.history.start({
