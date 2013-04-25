fbd330462d0900909684b91cab31a15859b4c5e4
Babichok <ababichok@morningcoffeesoft.com>
2013-04-25 17:45:23 +0300

    First model

---
+++ bones/public/app/models/Search.js
@@ -0,0 +1,4 @@
+var Search = Backbone.Model.extend({});
+var search_object = new Search(
+	{keywords: 'C++', id: 1}
+);
---
+++ bones/public/app/views/SearchView.js
@@ -0,0 +1,5 @@
+var Search_View = Backbone.View.extend({
+	render: function() {
+		$(this.el).html('<li>' + this.model.get('keywords')
+	}
+});
